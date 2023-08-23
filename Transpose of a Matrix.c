1d5e55597bf5cba1f92828bcc42f2d8d51488876
Swastik124 <142680486+Swastik124@users.noreply.github.com>
2023-08-23 15:26:25 +0530

    Add files via upload

---
+++ Transpose of a Matrix.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void input(int [][10],int,int);
+void display(int[][10],int,int);
+void transpose(int[][10],int,int);
+
+void main()
+{
+	int a[10][10],b[10][10],r1,r2,c1,c2;
+	printf("Enter the vcalues of r1,c1 and r2,c2 values respectively= ");
+	scanf("%d%d%d%d",&r1,&c1,&r2,&c2);
+	if(r1!=r2 || c1!=c2)
+	{
+		exit(0);
+	}
+	else
+	{
+		input(a,r1,c1);
+		display(a,r1,c1);
+		transpose(a,r1,c1);
+	}
+}
+
+void input(int a[10][10],int r1,int c1)
+{
+	printf("Enter matrix values= ");
+	int i,j;
+	for(i=0;i<r1;i++)
+	{
+		for(j=0;j<c1;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+	}
+}
+
+void display(int a[10][10],int r1,int c1)
+{
+	printf("Displaying matrix=\n");
+	int i,j;
+	for(i=0;i<r1;i++)
+	{
+		for(j=0;j<c1;j++)
+		{
+			 printf("\t%d",a[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void transpose(int a[10][10],int r1,int c1)
+{
+	printf("The transpose of matrix is=\n");
+	int c[10][10],i,j;
+	for(i=0;i<c1;i++)
+	{
+		for(j=0;j<r1;j++)
+		{
+			c[i][j]=a[j][i];
+		}
+	}
+	display(c,r1,c1);
+}
